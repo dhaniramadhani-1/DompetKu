10b3f52eea40dc1e745547128a69c1dd7e872ac7
dhaniramadhani-1 <dwi.yanto485@smk.belajar.id>
2026-09-23 14:42:55 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,23 +1,21 @@
-const CACHE_NAME = 'dompetku-cache-v1';
+const CACHE_NAME = 'keuangan-app-v1';
 const urlsToCache = [
   './',
   './index.html',
-  './style.css',
   './app.js',
   './manifest.json'
 ];
 
-// Install Service Worker & simpan file ke memori HP
+// Install Service Worker
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(urlsToCache);
     })
   );
-  self.skipWaiting();
 });
 
-// Ambil file dari memori HP saat offline
+// Fetch Cache
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
